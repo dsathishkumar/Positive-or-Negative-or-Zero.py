958614d07943b53e39f21511bb8aa0ac753bc85d
dsathishkumar <47025020+dsathishkumar@users.noreply.github.com>
2019-02-11 15:22:58 +0530

    Update phython.py

--- phython.py
+++ phython.py
@@ -1,7 +1,7 @@
-a = float(input("Enter a number: "))
-if a > 0:
+num = float(input("Enter a number: "))
+if num > 0:
    print("Positive number")
-elif a == 0:
+elif num == 0:
    print("Zero")
 else:
    print("Negative number")
